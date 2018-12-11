55321904383dae63f5cfef9ebd006e1e019f4db3
tmathobela <tmathobela@dvt.co.za>
2018-12-11 13:51:32 +0200

    first

--- src/cibautomationassessmentpt2/CIBAutomationAssessmentPt2.java
+++ src/cibautomationassessmentpt2/CIBAutomationAssessmentPt2.java
@@ -16,7 +16,7 @@ public class CIBAutomationAssessmentPt2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        // first
+        // first -
     }
     
 }
